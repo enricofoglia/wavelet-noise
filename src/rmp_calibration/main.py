f7957df14337d86ed79c67d1c998a4ee63d6df1b
Jose Rendón <jmrendona0417@gmail.com>
2026-03-23 21:17:08 -0400

    Added RMP PSD evaluation

--- src/rmp_calibration/main.py
+++ src/rmp_calibration/main.py
@@ -11,26 +11,35 @@ p_ref, p_rmp = load_calibration_data(file_path_calibration, file_name_calibratio
 time, p_target = load_experimental_data(file_path_experiments, file_name_experiments)
 f, H, Cxy, delay, group_delay, p_calibrated_t = calibrate_rmp_signal(p_ref, p_rmp, p_target, time)
 
+f,Pxx = signal.welch(p_calibrated_t, fs=1/(time[1] - time[0]), nperseg=1024)[1]
+
 print(40 * '-')
 print(f'Estimated delay: {delay:.4f} ms')
 print(40 * '-')
 
 plt.figure(figsize=(10, 8))
-plt.subplot(3, 1, 1)
+plt.subplot(4, 1, 1)
 plt.semilogx(f, 20 * np.log10(np.abs(H)))
 plt.title('Transfer Function (dB)')
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 plt.tight_layout()
 
-plt.subplot(3, 1, 2)
+plt.subplot(4, 1, 2)
 plt.semilogx(f, np.angle(H, deg=True))
 plt.title('Transfer Function Phase (degrees)')
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 plt.tight_layout()
 
-plt.subplot(3, 1, 3)
+plt.subplot(4, 1, 3)
 plt.semilogx(f, Cxy)
 plt.title('Coherence Quality Check')
 plt.ylim(0, 1.1)
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 plt.tight_layout()
+
+plt.subplot(4, 1, 4)
+plt.semilogx(f, 10 * np.log10(Pxx/4.0e-10))
+plt.title('Power Spectral Density (dB/Hz)')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.tight_layout()
+plt.show()
